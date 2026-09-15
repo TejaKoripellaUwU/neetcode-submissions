827b000b1f5fc36a7fcac6fdcf32a6ae32a6ce0e
Teja Koripella <73715703+TejaKoripellaUwU@users.noreply.github.com>
2026-09-15 13:32:59 -0500

    Add: car-pooling - submission-0

---
+++ Data Structures & Algorithms/car-pooling/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    struct tripEvent{
+        bool enter;
+        int ppl;
+        int time;
+    };
+    struct tripComp{
+        bool operator()(const tripEvent& a, const tripEvent& b){
+            if (a.time == b.time){
+                return a.enter;
+            }
+            return a.time > b.time;
+        }
+    };
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        std::priority_queue<tripEvent,std::vector<tripEvent>,tripComp> pq;
+        for (auto& e: trips){
+            std::cout<<e[1]<<" "<<e[2]<<std::endl;
+            pq.push(tripEvent{.enter = true, .ppl = e[0], .time = e[1]});
+            pq.push(tripEvent{.enter = false, .ppl = e[0], .time = e[2]});
+        }
+        int curppl = 0;
+        while (!pq.empty()){
+            auto event = pq.top();
+            pq.pop();
+            std::cout<< event.enter<<" "<< event.time<<" "<< event.ppl<< " "<< curppl<< std::endl;
+
+            if (event.enter){
+                curppl += event.ppl;
+            } else{
+                curppl -= event.ppl;
+            }
+            if (curppl > capacity){return false;}
+        }
+        return true;
+    }
+};
